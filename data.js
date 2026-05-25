a4e57ebc65ba9bb95a5fdf03583a5cab1c8d5be3
marcopavese <marco.pavese8@gmail.com>
2026-05-26 01:20:02 +0200

    Add files via upload

---
+++ data.js
@@ -0,0 +1,451 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-bagno7ok",
+      "name": "BAGNO7ok",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.1058862530342815,
+          "pitch": 0.3334674676684699,
+          "rotation": 0,
+          "target": "6-matrimoniale4ok"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-bagno8ok",
+      "name": "BAGNO8ok",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.6077946388639166,
+          "pitch": 0.3236107285981422,
+          "rotation": 0,
+          "target": "5-disimpegno9ok"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-cameretta3ok",
+      "name": "CAMERETTA3ok",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.7480325166424695,
+          "pitch": 0.15254678772990715,
+          "rotation": 0,
+          "target": "4-disimpegno6ok"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-cameretta5ok",
+      "name": "CAMERETTA5ok",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.3527488627354085,
+          "pitch": 0.25926777541759627,
+          "rotation": 0,
+          "target": "4-disimpegno6ok"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-disimpegno6ok",
+      "name": "DISIMPEGNO6ok",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.945352965592182,
+          "pitch": 0.2497088953878439,
+          "rotation": 0,
+          "target": "2-cameretta3ok"
+        },
+        {
+          "yaw": -1.5367572672472871,
+          "pitch": 0.2615093452511026,
+          "rotation": 0,
+          "target": "10-bagno11ok"
+        },
+        {
+          "yaw": -1.110476611374633,
+          "pitch": 0.27198910222609385,
+          "rotation": 0,
+          "target": "3-cameretta5ok"
+        },
+        {
+          "yaw": 2.0161442231362052,
+          "pitch": 0.2850710774182872,
+          "rotation": 0,
+          "target": "6-matrimoniale4ok"
+        },
+        {
+          "yaw": 0.8332085181148106,
+          "pitch": 0.5753828177982747,
+          "rotation": 0,
+          "target": "7-soggiorno2ok"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-disimpegno9ok",
+      "name": "DISIMPEGNO9ok",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.978438963799448,
+          "pitch": 0.17745799595632405,
+          "rotation": 0,
+          "target": "7-soggiorno2ok"
+        },
+        {
+          "yaw": 2.4550342253224535,
+          "pitch": 0.2672466013587531,
+          "rotation": 0,
+          "target": "1-bagno8ok"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-matrimoniale4ok",
+      "name": "MATRIMONIALE4ok",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.3806405876180747,
+          "pitch": 0.2812820674350398,
+          "rotation": 0,
+          "target": "0-bagno7ok"
+        },
+        {
+          "yaw": -0.6838475265734303,
+          "pitch": 0.22685743583919837,
+          "rotation": 0,
+          "target": "4-disimpegno6ok"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-soggiorno2ok",
+      "name": "SOGGIORNO2ok",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.9487511981880399,
+          "pitch": 0.2212972200939607,
+          "rotation": 0,
+          "target": "8-cucina1ok"
+        },
+        {
+          "yaw": 1.1405938466746193,
+          "pitch": 0.20936253194421717,
+          "rotation": 0,
+          "target": "5-disimpegno9ok"
+        },
+        {
+          "yaw": -2.9633654992505196,
+          "pitch": 0.23761413481360805,
+          "rotation": 0,
+          "target": "9-esterno10ok"
+        },
+        {
+          "yaw": -0.07307092110391977,
+          "pitch": -0.0007221705264388589,
+          "rotation": 0,
+          "target": "4-disimpegno6ok"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-cucina1ok",
+      "name": "CUCINA1ok",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1000,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.2157387709986665
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.31393775510133,
+          "pitch": 0.1879130637567954,
+          "rotation": 0,
+          "target": "7-soggiorno2ok"
+        },
+        {
+          "yaw": 1.8935788176287476,
+          "pitch": 0.07633943996734871,
+          "rotation": 0,
+          "target": "5-disimpegno9ok"
+        },
+        {
+          "yaw": 1.6241273616664174,
+          "pitch": 0.08491651771459985,
+          "rotation": 0,
+          "target": "4-disimpegno6ok"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-esterno10ok",
+      "name": "ESTERNO10ok",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.8845634082436824,
+          "pitch": 0.12133029778353155,
+          "rotation": 0,
+          "target": "7-soggiorno2ok"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "10-bagno11ok",
+      "name": "BAGNO11ok",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.1819348773008471,
+          "pitch": 0.17330342247730712,
+          "rotation": 0,
+          "target": "4-disimpegno6ok"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
